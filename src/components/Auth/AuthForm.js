063d0f08df1c8e07990efb79993b94958de16e70
Gulyapas Poonkawinsiri <gulyapas.p@gmail.com>
2022-06-11 18:51:14 +0700

    add autoLogout

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -61,7 +61,7 @@ const AuthForm = () => {
         }
       })
       .then((data) => {
-        authCtx.login(data.idToken);
+        authCtx.login(data.idToken, data.expiresIn);
         history.replace('/')
       })
       .catch((err) => {
--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -7,20 +7,29 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expiresIn) => {
+  const curTime = new Date().getTime();
+  const expiresTime = new Date(curTime + expiresIn * 1000);
+  return expiresTime - curTime;
+};
+
 export const AuthContextProvider = (props) => {
   const initialState = localStorage.getItem("token");
   const [token, setToken] = useState(initialState);
 
   const userIsLoggedIn = !!token; // return true if token is a string, false if token is null
 
-  const loginHandler = (token) => {
-    localStorage.setItem("token", token);
-    setToken(token);
-  };
-
   const logoutHandler = () => {
     localStorage.removeItem("token");
     setToken(null);
+  };
+
+  const loginHandler = (token, expiresIn) => {
+    localStorage.setItem("token", token);
+    setToken(token);
+
+    const remainingTime = calculateRemainingTime(expiresIn);
+    setTimeout(logoutHandler, remainingTime);
   };
 
   const contextValue = {
